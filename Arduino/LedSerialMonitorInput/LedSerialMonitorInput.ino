d847746f0282a2169337bc6bee19bcfa771dec16
snugghash <snugghash@gmail.com>
2017-01-24 15:58:10 -0500

    Serial monitor milticolor control

--- Arduino/LedSerialMonitorInput/LedSerialMonitorInput.ino
+++ Arduino/LedSerialMonitorInput/LedSerialMonitorInput.ino
@@ -1,21 +1,48 @@
 
-int buttonIn = 5;
-int ledOut = 7;
-
+int pinRed = 7;
+int pinYellow = 8;
+int pinGreen = 9;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledOut, OUTPUT);
-  pinMode(buttonIn, INPUT);
-  digitalWrite(buttonIn, HIGH);
+  pinMode(pinRed, OUTPUT);
+  pinMode(pinGreen, OUTPUT);
+  pinMode(pinYellow, OUTPUT);
   
+  Serial.begin(9600);  
+  digitalWrite(pinRed, LOW);
+  digitalWrite(pinYellow, LOW);
+  digitalWrite(pinGreen, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int val = digitalRead(buttonIn);
-  if (val == LOW)
-    digitalWrite(ledOut, HIGH);
-  else
-    digitalWrite(ledOut, LOW);
+  //Check if some input is stored in buffer
+  if(Serial.available()) {
+    char input = Serial.read();
+    if (input == 'R') {
+      if (digitalRead(pinRed) == HIGH)
+        digitalWrite(pinRed,LOW);
+      else
+        digitalWrite(pinRed, HIGH);
+    }
+    if (input == 'Y') {
+      if (digitalRead(pinYellow) == HIGH)
+        digitalWrite(pinYellow,LOW);
+      else
+        digitalWrite(pinYellow, HIGH);
+    }
+    if (input == 'G') {
+      if (digitalRead(pinGreen) == HIGH)
+        digitalWrite(pinGreen,LOW);
+      else
+        digitalWrite(pinGreen, HIGH);
+    }
+    else if(input == 'O') {
+      digitalWrite(pinRed, LOW);
+      digitalWrite(pinGreen,LOW);
+      digitalWrite(pinYellow,LOW);
+    }
+    else 
+      digitalWrite(pinRed, LOW);
+  }
   //delay(1000);
 }
